ce2b17893536627aba3a2088091dbca5ccc42c27
Julien Dugauquier <julien.dugauquier@neuf.fr>
2022-05-11 20:05:06 +0200

    deployf

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -3,7 +3,7 @@ import navbar from "./navbar";
 import sidebar from "./sidebar";
 
 export default hopeTheme({
-  hostname: "https://a670485.github.io/DigitalProduction/",
+  hostname: "https://a670485.github.io/siteTest/",
 
   author: {
     name: "Dugauquier Julien",
